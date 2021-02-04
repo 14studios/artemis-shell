4681ac6a61be74e20f1e25a4459c57e484bdcd0e
Danny H <git@bean.codes>
2021-02-03 22:18:33 -0800

    Changes
    - Added artemis-updater and version commands
    - Added dynamics to header

--- setup/deploy.sh
+++ setup/deploy.sh
@@ -1,4 +1,6 @@
+#!/bin/sh
 WHICHID=$(ls /usr/bin/ | grep which)
+VERSION="1.1.0"
 which awk > /dev/null
 AWKID=$?
 which curl > /dev/null
@@ -44,32 +46,44 @@ fi
 read -p "Are you sure you want to continue? This may damage your system. [Y/n] " prompt
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
 then
+    # Create artemisctl directory
+    mkdir -p /etc/artemisctl
+    # Add version to /etc/artemisctl/version
+    echo "$VERSION" > /etc/artemisctl/version
+    # Clean all files
     /usr/bin/env rm -rf /etc/update-motd.d/*
+    /usr/bin/env rm -rf /etc/banner
+    /usr/bin/env rm -rf /etc/profile
+    /usr/bin/env rm -rf /etc/bash.bashrc
+    /usr/bin/env rm -rf /usr/bin/version
+    /usr/bin/env rm -rf /usr/lib/command-not-found
+    /usr/bin/env rm -rf /etc/skel/.bashrc
+    /usr/bin/env rm -rf /usr/lib/python3/dist-packages/CommandNotFound/CommandNotFound.py
     /usr/bin/env mkdir -p /var/cache/motd-news.d/
+    # Download assets
     /usr/bin/env curl https://raw.githubusercontent.com/14studios/artemis-shell/master/ssh/update-motd.d/00-introduction --output /etc/update-motd.d/00-introduction
     /usr/bin/env curl https://raw.githubusercontent.com/14studios/artemis-shell/master/ssh/update-motd.d/10-header --output /etc/update-motd.d/10-header
     /usr/bin/env curl https://raw.githubusercontent.com/14studios/artemis-shell/master/ssh/update-motd.d/20-help-text --output /etc/update-motd.d/20-help-text
     /usr/bin/env curl https://raw.githubusercontent.com/14studios/artemis-shell/master/ssh/update-motd.d/50-motd-news --output /etc/update-motd.d/50-motd-news
-    /usr/bin/dos2unix /etc/update-motd.d/* > /dev/null
-    /usr/bin/env chmod +x /etc/update-motd.d/*
-    /usr/bin/env rm -rf /etc/banner
     /usr/bin/env curl https://raw.githubusercontent.com/14studios/artemis-shell/master/ssh/etc/banner --output /etc/banner
-    /usr/bin/dos2unix /etc/banner > /dev/null
-    /usr/bin/env rm -rf /etc/profile
     /usr/bin/env curl https://raw.githubusercontent.com/14studios/artemis-shell/master/shell/profile --output /etc/profile
-    /usr/bin/dos2unix /etc/profile > /dev/null
-    /usr/bin/env rm -rf /etc/bash.bashrc
     /usr/bin/env curl https://raw.githubusercontent.com/14studios/artemis-shell/master/shell/bash.bashrc --output /etc/bash.bashrc
-    /usr/bin/dos2unix /etc/bash.bashrc > /dev/null
-    /usr/bin/env rm -rf /usr/lib/command-not-found
     /usr/bin/env curl https://raw.githubusercontent.com/14studios/artemis-shell/master/shell/command-not-found/usr-lib/command-not-found --output /usr/lib/command-not-found
-    /usr/bin/dos2unix /usr/lib/command-not-found > /dev/null
-    /usr/bin/env rm -rf /usr/lib/python3/dist-packages/CommandNotFound/CommandNotFound.py
     /usr/bin/env curl https://raw.githubusercontent.com/14studios/artemis-shell/master/shell/command-not-found/usr-lib/python3-distpkgs/CommandNotFound.py --output /usr/lib/python3/dist-packages/CommandNotFound/CommandNotFound.py
-    /usr/bin/dos2unix /usr/lib/python3/dist-packages/CommandNotFound/CommandNotFound.py > /dev/null
-    /usr/bin/env rm -rf /etc/skel/.bashrc
     /usr/bin/env curl https://raw.githubusercontent.com/14studios/artemis-shell/master/shell/skel/.bashrc --output /etc/skel/.bashrc
+    /usr/bin/env curl https://raw.githubusercontent.com/14studios/artemis-shell/master/shell/commands/version --output /usr/bin/version
+    # Update permissions
+    /usr/bin/env chmod +x /etc/update-motd.d/*
+    /usr/bin/env chmod +x /usr/bin/version
+    # Convert all files from Windows to Unix format
     /usr/bin/dos2unix /etc/skel/.bashrc > /dev/null
+    /usr/bin/dos2unix /etc/update-motd.d/* > /dev/null
+    /usr/bin/dos2unix /usr/bin/version > /dev/null
+    /usr/bin/dos2unix /usr/lib/python3/dist-packages/CommandNotFound/CommandNotFound.py > /dev/null
+    /usr/bin/dos2unix /usr/lib/command-not-found > /dev/null
+    /usr/bin/dos2unix /etc/bash.bashrc > /dev/null
+    /usr/bin/dos2unix /etc/profile > /dev/null
+    /usr/bin/dos2unix /etc/banner > /dev/null
 else
   exit 0
 fi
@@ -86,5 +100,12 @@ else
     /usr/bin/dos2unix /etc/ssh/sshd_config > /dev/null
 fi
 
-read -p "The script has been deployed! Press any key to exit.. "
+# Download Artemis Updater
+echo "Installing Artemis Updater.."
+/usr/bin/env rm -rf /usr/bin/artemis-updater
+/usr/bin/env curl https://raw.githubusercontent.com/14studios/artemis-shell/master/shell/commands/artemis-updater --output /usr/bin/artemis-updater
+/usr/bin/env chmod +x /usr/bin/artemis-updater
+/usr/bin/dos2unix /usr/bin/artemis-updater > /dev/null
+
+read -p "Artemis Shell has been installed. Press any key to exit.. "
 exit 0
